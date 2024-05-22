0c07a10cdaec3b23ff2099695d95c0e10989072b
Sophie Sun <sophie24sun@gmail.com>
2024-05-22 19:47:48 +0100

    #1 Risk for optimal hard threshold

--- R/calculate_max_regret.R
+++ R/calculate_max_regret.R
@@ -57,10 +57,28 @@ calculate_max_regret <- function(VR, VU, VUR) {
                                     + (x_b < -l) * x_b - matrix(b_grid, sims, Kb, byrow = TRUE))^2) / sims
   
   risk_function_ht_ttest <- corr^2 * Ebsims_ht(1.96) + 1 - corr^2
+  # Use analytical formula to calculate the risk function for the pre-test estimator that switches between Y_U and GMM
+  Eb_ht <- function(l) {
+    result <- 1+(b_grid^2-1) *(pnorm(l-b_grid)-pnorm(-l-b_grid))+ 
+      (l-b_grid) *dnorm(l-b_grid) - (-l-b_grid) *dnorm(-l-b_grid)
+    return(result)
+  }
+
+  risk_function_ht_ttest <-   corr^2 *Eb_ht(1.96) + 1 - corr^2
+  
                          
   # Calculate max regret for various estimators
   max_regret_ttest <- max(risk_function_ht_ttest / risk_oracle)
   message('The pre-test estimator has max regret\n', round(max_regret_ttest,2), '\n')
+  
+  # Interpolate the hard-threshold estimate based on the estimated correlation coeff
+  ht.function <- splinefun(Sigma_UO_grid, thresholds$ht.mat, method = "fmm", ties = mean)
+  ht <- ht.function(abs(corr))
+   
+  risk_function_ht_adaptive <- corr^2 * Eb_ht(ht) + 1 - corr^2 
+  
+  max_regret_ht <- max(risk_function_ht_adaptive / risk_oracle)
+  message('The adaptive hard-threshold estimator has max regret\n', round(max_regret_ht,2), '\n')
   # Calculate the adaptive ERM lambda
   lambda.function <-  splinefun(Sigma_UO_grid, mse$MSE.lambda.mat, method = "fmm", ties = mean)
   lambda <- lambda.function(abs(corr))
@@ -77,6 +95,8 @@ calculate_max_regret <- function(VR, VU, VUR) {
     max_regret_nonlinear = max_regret_nonlinear,
     max_regret_st = max_regret_st,
     st = st,
+    max_regret_ht = max_regret_ht,
+    ht = ht,
     max_regret_ttest = max_regret_ttest,
     max_regret_erm = max_regret_erm,
     max_regret_adaptive_erm = max_regret_adaptive_erm,
@@ -84,6 +104,7 @@ calculate_max_regret <- function(VR, VU, VUR) {
     lambda = lambda,
     max_risk_adaptive = max(risk_function_adaptive),
     max_risk_st_adaptive = max(risk_function_st_adaptive),
+    max_risk_ht_adaptive = max(risk_function_ht_adaptive),
     max_risk_ht_ttest = max(risk_function_ht_ttest),
     max_risk_erm = max(risk_function_erm),
     max_risk_adaptive_erm = max(risk_function_adaptive_erm)
